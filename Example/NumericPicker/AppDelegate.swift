421897def8397d4de75b3eb75d2643ab5d0ca5f2
Matt Lewin <matt@lewin.us>
2017-02-02 16:47:52 -0800

    Strip useless code and whitespace

--- Example/NumericPicker/AppDelegate.swift
+++ Example/NumericPicker/AppDelegate.swift
@@ -10,16 +10,10 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 }
