802a66ff26c3ed88c57afdc42973a63a1ae8c67d
Matt Lewin <matt@lewin.us>
2017-10-14 16:03:53 -0700

    Comment out the entirety of Package.swift to prevent Xcode from complaining that `PackageDescription` doesn't exist

--- Package.swift
+++ Package.swift
@@ -1,7 +1,9 @@
 // NOTE: The Swift Package Manager does not currently support UIKit or iOS. Thus this file is of no use. It is here for
 // future use.
+/*
 import PackageDescription
 
 let package = Package(
     name: "NumericPicker"
 )
+*/
